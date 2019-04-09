74cb7b26cff3a8d789b6ef056e2e062629777bf3
lovetrivedi <lovelyjoshi93@gmail.com>
2019-04-09 19:00:58 +0530

    conversation in redis

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-const io = require('socket.io')(8080)
+const io = require('socket.io')(8080);
+//var socket = io('http://localhost');
+const redis = require('redis');
+const Conversation = require('../model/main')
+
+const conversation = new Conversation()
+
 
 let users = []
 
@@ -6,9 +12,8 @@ const findUser = username => users.find(user => user.username == username)
 
 io.on('connection', socket => {
 
-    console.log('connected')
-
     socket.on('newConnection', data => {
+      
         users.push({
             username: data.username,
             socketId: socket.id
@@ -16,10 +21,11 @@ io.on('connection', socket => {
     })
 
     socket.on('sendMessage', data => {
-        console.log(users)
-        const user = findUser(data.to)
-        console.log(user)
-        if (user) socket.broadcast.to(user.socketId).emit('receivedMessage', data)
+      const conversation_id = conversation.set_conv_id(data.author, data.to)//create conv ID
+      console.log(conversation_id)
+      conversation.save_message(conversation_id, data.content) // SAVE MESSAGE
+      const user = findUser(data.to)
+      if (user) socket.broadcast.to(user.socketId).emit('receivedMessage', data)
     })
 
     socket.on('disconnect', () => {
