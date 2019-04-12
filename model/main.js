747af37321bd7c3e6f28dedab8457f118a96ec94
lovetrivedi <lovelyjoshi93@gmail.com>
2019-04-12 18:44:19 +0530

    displaying chat to frontend

---
+++ model/main.js
@@ -0,0 +1,166 @@
+
+const redis = require('redis');
+
+let client = redis.createClient();
+client.on('connect', ()=>{
+
+})
+
+module.exports = class Conversation {
+
+  //--------- CREATE CONVERSATION ID -----------------------/////
+ set_conv_id(author , to ) {
+
+    let conversation_id =  author.concat('#'+to);
+    let participates = author.concat(','+to);
+
+    let participates1 = participates.split(',')
+    let participates2 = participates1[1].concat(',',participates1[0]);
+
+    let conversation_id1 = conversation_id.split('#')
+    let conversation_id2 = conversation_id1[1].concat('#',conversation_id1[0]);
+
+    //console.log("1st id ==>",conversation_id)
+    //console.log("2nd id==>",conversation_id2)
+
+    client.lrange("conversation"+participates, 0, -1,
+    (err,data) =>   {
+     if(err){
+         console.log(err)
+     }else{
+       console.log("first data[1]==> ", data[1])
+       if (data[1] !== conversation_id ) {
+         client.lrange("conversation"+participates2, 0, -1,
+         (err,data) =>{
+           if(err){console.log("err")}
+           else{
+             console.log("second data[1]==>",data[1])
+             if(data[1] == conversation_id2 ){
+               console.log("conversation_id already exist")
+
+
+             }else{
+               client.rpush("conversation"+participates,participates,conversation_id )
+               console.log("conversation_id created")
+
+        }  }  })
+
+     }else{
+
+       console.log("conversation_id already exist")
+     }
+   }
+   })
+
+ }////////////////// method end ///////////////
+
+  ////////============== GET CONVERSATION ID ================////////////
+    get_conv_id(author, to){
+      return new Promise((resolve, reject)=>{
+
+        var conversationId;
+        let participates = author.concat(','+to);
+        let participates1 = participates.split(',')
+        let participates2 = participates1[1].concat(',',participates1[0]);
+
+         client.lrange("conversation"+participates, 0, -1,
+          (err,data) =>
+          {
+            if(err){
+                console.log(err)
+                reject(err)
+            }else{
+               if (typeof(data[1]) != "undefined"){
+                  resolve(data[1])
+                  //resolve({author:author, to:to, conv:data[1]})
+                 console.log(data[1])
+
+               }else{
+                 client.lrange("conversation"+participates2, 0, -1,(err,data)=>{
+                   if(err){
+                     console.log(err)
+                     reject(err)
+                   }else{
+                     if (typeof(data[1]) != "undefined"){
+                      resolve(data[1])
+                      //resolve({author:autdata[1]})
+                       console.log(data[1])
+                     }else{console.log("no id")}
+                   }
+                 })
+               }
+              }
+          })
+
+      })
+
+   }//////////////// method end ///////////////////
+
+//===================  SAVE MESSAGE   ==================//////////////////////
+
+save_message(author, to, conversation_id, content){
+
+    const data ={
+      author:author,
+      to:to,
+      content:content
+    }
+    client.rpush("message"+conversation_id, JSON.stringify(data));
+    client.lrange("message"+conversation_id , 0, -1,
+    (err,data) =>  {
+      if(err){
+          console.log(err)
+      }else{
+        //console.log(data)
+        console.log("message saved")
+        //console.log(response)
+    }
+  })
+} //////////// method end /////////////////////
+
+//>>>>>>>>>>>>>>>>>>>>> GET  MESSAGES <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+get_message(conversation_id){
+  let i = 0;
+  client.lrange("message"+conversation_id, 0, -1,(err,response) =>
+  {
+    if(err){
+          console.log(err)
+    }else{
+      //return response;
+      console.log(response)
+      const l = response.length
+      for( i = 0; i < l; i++){
+      //  console.log(JSON.parse(response[i]).author_id)
+        console.log(JSON.parse(response[i]).content)
+      //  console.log(JSON.parse(response[i]).to_id)
+        console.log("  ")
+     }
+    }
+    })
+  } ////////// method end ////////////////////////////
+
+  ///-----------------------DELETE -----------------////////////
+
+delete_message(conversation_id){
+
+      client.del("message"+conversation_id,(err, data)=>{
+        if(err){
+          console.log(err)
+        }else{
+          console.log("message deleted")
+        }
+      })
+  } /////// method end ////////////////
+
+delete_conversation_id(participates){
+  client.del("conversation"+participates,(err, data)=>{
+    if(err){
+      console.log('err')
+    }else{
+      console.log('deleted')
+    }
+  })
+}
+
+
+} //////////////////// CLASS END /////////////////////////////////////
--- server/index.js
+++ server/index.js
@@ -1,12 +1,59 @@
 const io = require('socket.io')(8080)
+const express = require('express')
+const app = express()
+const redis = require('redis');
+const Conversation = require('../model/main')
+
+let client = redis.createClient();
+client.on('connect', ()=>{
+
+})
+
+const conversation = new Conversation()
+
+// conversation.delete_message('love#trivedi')
+// conversation.delete_conversation_id('love,trivedi')
+// conversation.delete_message('trivedi#love')
+// conversation.delete_conversation_id('trivedi,love')
 
 let users = []
+
+app.get('/message', (req, res, next ) =>{
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  conversation_id = 'love#trivedi'
+  client.lrange("message"+conversation_id, 0, -1,
+  (err,data) =>{
+    if(err){
+        res.json({message: " error "});
+        console.log(err);
+    }else{ 
+      
+        //console.log(JSON.parse(data))
+         let i =0;
+         let messagedata = [];
+         for (i = 0; i< data.length; i++){
+             messagedata.push(JSON.parse(data[i]))
+         }
+         console.log(messagedata)
+         res.send({data:messagedata})
+ }
+
+    
+  })
+})
+
+// app.get('/message', (req, res, next ) =>{
+//     res.send({message: " test "}); 
+//   })
 
 const findUser = username => users.find(user => user.username == username)
 
 io.on('connection', socket => {
 
-    console.log('connected')
+
 
     socket.on('newConnection', data => {
         users.push({
@@ -16,7 +63,14 @@ io.on('connection', socket => {
     })
 
     socket.on('sendMessage', data => {
-        console.log(users)
+
+
+        conversation.set_conv_id(data.author, data.to)
+        conversation.get_conv_id(data.author, data.to)
+       .then(conv=> {conversation.save_message(data.author, data.to, conv, data.content)})
+       //.then(data=> {console.log("response ==>",data.author)})
+
+
         const user = findUser(data.to)
         console.log(user)
         if (user) socket.broadcast.to(user.socketId).emit('receivedMessage', data)
@@ -26,3 +80,4 @@ io.on('connection', socket => {
         users = users.filter(user => user.socketId !== socket.id)
     })
 })
+app.listen(3030, () => console.log(`API  listening`))
